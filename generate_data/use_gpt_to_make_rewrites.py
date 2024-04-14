c65239e9a9a3dc703669b18bfb5e691edc227992
Alex Saavedra <alexsaavedra2023@u.northwestern.edu>
2024-04-14 17:16:55 -0500

    gpt bigrams with vectors

--- generate_data/use_gpt_to_make_rewrites.py
+++ generate_data/use_gpt_to_make_rewrites.py
@@ -1,14 +1,14 @@
 #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
 from openai import OpenAI
 client = OpenAI(api_key="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")
-def get_new_prompt(old_prompt):
+def get_new_prompt(rewrite_prompt, original_text):
     
     completion = client.chat.completions.create(
     
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a super intelligent AI. I am going to give you a garbled llm prompt, and you must tell me the prompt i meant to say. Each prompt will be some instruction to transform or rewrite a piece of text. Change as little as possible to make it meaningful"},
-        {"role": "user", "content": old_prompt}
+        {"role": "system", "content": "You are a super intelligent AI. Please follow the instructions carefully."},
+        {"role": "user", "content": rewrite_prompt + " " +  original_text}
     ]
     )
     return completion.choices[0].message.content
@@ -25,21 +25,34 @@ def has_non_alphanumeric(row):
             return True
     return False
 def string_not_empty(row):
-     return len(row) > 0 
+     if type(row) == type("string"):
+        return len(row) > 0 
+     return False
 
 # Apply the function to the 'Text' column and filter rows
-df = df[df['rewrite_prompt'].apply(lambda x: not string_not_empty(x))]
+df = df[df['rewrite_prompt'].apply(lambda x: string_not_empty(x))]
+print(df.head(3))
+df = df[df['original_text'].apply(lambda x: string_not_empty(x))]
 print(df.head(3))
 import time
 import csv
 
-with open('./gpt_bigram_v4.csv', mode='w', newline='') as file:
-            writer = csv.writer(file)
+with open('./gpt_bigram_v4.csv', mode='w', newline='', encoding='latin-1') as file:
             
-            for index, row in df.iterrows():
-                new_prompt = get_new_prompt(row['rewrite_prompt_old'])
-                row['rewrite_prompt'] = new_prompt
-                row['file'] = 'bigram_mean_v2'
-                writer.writerow(row.to_list())
-                time.sleep(0.15)
-                print(row["rewrite_prompt"])
+                writer = csv.writer(file)
+                
+                for index, row in df.iterrows():
+                    try:
+                        rewritten_text = get_new_prompt(row['rewrite_prompt'], row['original_text'])
+                        row['rewritten_text'] = rewritten_text
+                        row['file'] = 'gpt_rewrittens_v15'
+                        writer.writerow(row.to_list())
+                        time.sleep(0.15)
+                        print("\nog", row['original_text'])
+                        print("\nrp", row['rewrite_prompt'])
+                        print("\nog", row['rewritten_text'])
+                    except:
+                        print(f"ROW {index} FAILED! Moving on!")
+
+
+                    
